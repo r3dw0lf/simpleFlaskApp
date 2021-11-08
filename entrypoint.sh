3822ed2f08e3448bc1f58d2e7fb300d06983aa5d
Ali Akbar Mohammadi <ali-akbar@live.com>
2021-11-08 19:09:38 +0300

    search and get the parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [[ -z "${SECRET_PATH}" ]]; then
+  SECRET_STORE="/mnt/"
+else
+  SECRET_STORE="${SECRET_PATH}"
+fi
 
-for i in `find /opt/ -name "*.json" -type f`; do
+for i in `find ${SECRET_STORE} -name "*.json" -type f`; do
     eval $(jq -r 'to_entries | .[] | "export " + .key + "=" + (.value | @sh)' < $i)
 done
 
-
 exec "$@"
+
